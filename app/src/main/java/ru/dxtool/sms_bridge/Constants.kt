1c54313dafd5d79a76ab23ecd1d637057a63074a
Aleksei <mr.kolupaev@gmail.com>
2025-10-04 15:35:57 +0800

    Changed default settings

--- app/src/main/java/ru/dxtool/sms_bridge/Constants.kt
+++ app/src/main/java/ru/dxtool/sms_bridge/Constants.kt
@@ -1,7 +1,7 @@
 package ru.dxtool.sms_bridge.util
 
 object Constants {
-    const val DEFAULT_WEBHOOK_URL = "https://www.feishu.cn/flow/api/trigger-webhook/173f273036c25fa9a5251c36723e6425"
+    const val DEFAULT_WEBHOOK_URL = "https://..."
     const val WEBHOOK_PROMPT = "Enter Webhook URL"
     const val START_SERVICE = "Start Service"
     const val SERVICE_RUNNING = "Service Running"
